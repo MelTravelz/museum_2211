552493d7b5f98e8536aca455d1855e10af361652
Melony Franchini <meltravelz@gmail.com>
2023-01-03 09:26:32 -0700

    test: Add initialize tests for museum class

---
+++ spec/museum_spec.rb
@@ -0,0 +1,19 @@
+require 'rspec'
+require './lib/exhibit'
+require './lib/patron'
+require './lib/museum'
+
+RSpec.describe Museum do
+  let (:dmns) { Museum.new("Denver Museum of Nature and Science") }
+
+  describe "#initialize" do
+    it "exists" do
+      expect(dmns).to be_instance_of(Museum)
+    end
+
+    it "has attributes" do
+      expect(dmns.name).to eq("Denver Museum of Nature and Science")
+      expect(dmns.exhibits).to eq([])
+    end
+
+end
