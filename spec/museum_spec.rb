53412d96ca0bd2c2a508094faf78c88d578f856e
Melony Franchini <meltravelz@gmail.com>
2023-01-03 09:33:14 -0700

    test: Add add_exhibit test for museum class

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -4,7 +4,10 @@ require './lib/patron'
 require './lib/museum'
 
 RSpec.describe Museum do
-  let (:dmns) { Museum.new("Denver Museum of Nature and Science") }
+  let(:dmns) { Museum.new("Denver Museum of Nature and Science") }
+  let(:gems_and_minerals) { Exhibit.new({name: "Gems and Minerals", cost: 0}) }
+  let(:dead_sea_scrolls) { Exhibit.new({name: "Dead Sea Scrolls", cost: 10}) }
+  let(:imax) { Exhibit.new({name: "IMAX",cost: 15}) }
 
   describe "#initialize" do
     it "exists" do
@@ -15,5 +18,15 @@ RSpec.describe Museum do
       expect(dmns.name).to eq("Denver Museum of Nature and Science")
       expect(dmns.exhibits).to eq([])
     end
+  end
 
+  describe "#add_exhibit" do
+    it "can add exhibit to the exhibits array" do
+      dmns.add_exhibit(gems_and_minerals)
+      dmns.add_exhibit(dead_sea_scrolls)
+      dmns.add_exhibit(imax)
+      
+      expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
+    end
+  end
 end
