7e3094265f5f5354d724db393b8915cd70e493a1
Melony Franchini <meltravelz@gmail.com>
2023-01-03 09:08:37 -0700

    test: Add initialize tests

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -1,6 +1,19 @@
 require 'rspec'
 require './lib/exhibit'
+require './lib/patron'
 
 RSpec.describe Exhibit do
+  let(:exhibit) { Exhibit.new({name: "Gems and Minerals", cost: 0}) }
+
+  describe "#initialize" do
+    it "exists" do
+      expect(exhibit).to be_instance_of(Exhibit)
+    end
+
+    it "has attributes" do 
+      expect(exhibit.name).to eq("Gems and Minerals")
+      expect(exhibit.cost).to eq(0)
+    end
+  end
   
 end
