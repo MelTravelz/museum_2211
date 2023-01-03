563eb29e1f6fb8db2eb9b96f02e9c264226b5699
Melony Franchini <meltravelz@gmail.com>
2023-01-03 10:32:54 -0700

    test: Add ppatrons_by_exhibit_interest test for museum class

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -70,4 +70,25 @@ RSpec.describe Museum do
       expect(dmns.patrons).to eq([patron_1, patron_2, patron_3])
     end
   end
+
+  describe "#patrons_by_exhibit_interest" do
+    it "can admit patrons who have an interest in exhibit" do
+      dmns.add_exhibit(gems_and_minerals)
+      dmns.add_exhibit(dead_sea_scrolls)
+      dmns.add_exhibit(imax)
+
+      patron_1.add_interest("Gems and Minerals")
+      patron_1.add_interest("Dead Sea Scrolls")
+      patron_2.add_interest("Dead Sea Scrolls")
+      patron_3.add_interest("Dead Sea Scrolls")
+
+      expected_hash = {
+        gems_and_minerals => [patron_1],
+        dead_sea_scrolls => [patron_1, patron_2, patron_3],
+        imax => []
+      }
+
+      expect(dmns.patrons_by_exhibit_interest).to eq(expected_hash)
+    end
+  end
 end
